4f35e197878efad210e1b948319770f2ead79cb4
imagire <imagire@gmail.com>
2021-09-17 15:09:31 +0900

    add files

---
+++ src/ConsoleApplication/ConsoleApplication.cpp
@@ -0,0 +1,34 @@
+﻿#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーからほとんど使用されていない部分を除外する
+#include "Windows.h"                    // Windows API の機能定義
+
+#include <stdio.h>   // printf
+#include <stdlib.h>   // malloc
+#include <stdbool.h> // bool
+
+#include "../include/lib_func.h"
+
+
+int main()
+{
+	int val1, val2, val3, sum;
+
+	val1 = 1;
+	val2 = 2;
+	printf("%dと%dは%s\n", val1, val2, is_same(val1, val2) ? "等しい" : "等しくない");
+
+	val1 = 0;
+	val2 = 0;
+	printf("%dと%dは%s\n", val1, val2, is_same(val1, val2) ? "等しい" : "等しくない");
+
+	sum = 2;
+	val1 = 1;
+	val2 = 1;
+	val3 = 1;
+	printf("%dと%dと%dの任意の数の和が%dになる組み合わせは%d通りです\n", val1, val2, val3, sum, calc_combinations_3(sum, val1, val2, val3));
+
+	sum = 3;
+	val1 = 1;
+	val2 = 2;
+	val3 = 3;
+	printf("%dと%dと%dの任意の数の和が%dになる組み合わせは%d通りです\n", val1, val2, val3, sum, calc_combinations_3(sum, val1, val2, val3));
+}
---
+++ src/StaticLib/StaticLib.c
@@ -0,0 +1,20 @@
+﻿#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーからほとんど使用されていない部分を除外する
+#include "Windows.h"                    // Windows API の機能定義
+
+#include <stdbool.h>
+
+#include "../include/lib_func.h"
+
+// 2つの数字が等しいですか？
+bool is_same(int val1, int val2)
+{
+	// やること：この処理を数字を比較するように変更します
+	return false;
+}
+
+// val1 から val3 の任意の組み合わせでsumになる組み合わせは何個ありますか？
+int calc_combinations_3(int sum, int val1, int val2, int val3)
+{
+	// やること：val1～val3の和がsumになる数字の組み合わせの数を計算します
+	return 0;
+}
