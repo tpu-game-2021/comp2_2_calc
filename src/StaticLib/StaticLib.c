79600bc5daf4d8d5c7ec52f1356a0ff7dc3e9fbf
funoji <railgun0000@outlook.jp>
2021-09-26 00:39:37 +0900

    テストを通しました

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -9,12 +9,62 @@
 bool is_same(int val1, int val2)
 {
 	// やること：この処理を数字を比較するように変更します
-	return false;
+	{
+		if (val1 == val2)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 }
 
 // val1 から val3 の任意の組み合わせでsumになる組み合わせは何個ありますか？
 int calc_combinations_3(int sum, int val1, int val2, int val3)
 {
 	// やること：val1～val3の和がsumになる数字の組み合わせの数を計算します
-	return 0;
+	int a, b, c, d, e, i;
+	int count;
+
+
+	a = val1 + val2;
+	b = val1 + val3;
+	c = val2 + val3;
+	e = val1 + val2 + val3;
+	
+
+
+		count = 0;
+		if (sum == val1)
+		{
+			count++;
+		}
+		if (sum == val2)
+		{
+			count++;
+		}
+		if (sum == val3)
+		{
+			count++;
+		}
+		if (sum == a)
+		{
+			count++;
+		}
+		if (sum == b) 
+		{
+			count++;
+		}
+		if (sum == c)
+		{
+			count++;
+		}
+		if (sum == e)
+		{
+			count++;
+		}
+		return count;
+	
 }
--- src/include/lib_func.h
+++ src/include/lib_func.h
@@ -9,8 +9,10 @@ extern "C" {
 // 2つの数字が等しいですか？
 bool is_same(int val1, int val2);
 
+
 // val1 から val3 の任意の組み合わせで値の和がsumになる組み合わせは何個ありますか？
 int calc_combinations_3(int sum, int val1, int val2, int val3);
+
 
 // C++ でCのライブラリを使うときのおまじない
 #ifdef __cplusplus
