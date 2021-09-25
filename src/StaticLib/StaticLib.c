0034be75c015bce97c2dbcc7e29904d18da6c05c
futoi7 <g2127030@st.t-kougei.ac.jp>
2021-09-26 01:16:27 +0900

    テストを通せませんでした。

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -1,5 +1,5 @@
 ﻿#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーからほとんど使用されていない部分を除外する
-#include "Windows.h"                    // Windows API の機能定義
+#include "Windows.h"// Windows API の機能定義
 
 #include <stdbool.h>
 
@@ -8,6 +8,9 @@
 // 2つの数字が等しいですか？
 bool is_same(int val1, int val2)
 {
+	if(val1 == val2);
+
+
 	// やること：この処理を数字を比較するように変更します
 	return false;
 }
@@ -15,6 +18,10 @@ bool is_same(int val1, int val2)
 // val1 から val3 の任意の組み合わせでsumになる組み合わせは何個ありますか？
 int calc_combinations_3(int sum, int val1, int val2, int val3)
 {
+	for (val1 = 1; val1 < 4; val1++) 
+		for (val2 = 1; val2 < 4; val2++) 
+			for (val3 = 1; val3 < 4; val3++) 
+				if (val1 + val2 + val3 == sum) 
 	// やること：val1～val3の和がsumになる数字の組み合わせの数を計算します
 	return 0;
 }
