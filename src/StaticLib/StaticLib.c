730163cb2b65b1748e2743d31ff4c77790e90309
KemonoMario <jon0902@icloud.com>
2021-09-25 12:32:27 +0900

    テストを通しました

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -8,13 +8,30 @@
 // 2つの数字が等しいですか？
 bool is_same(int val1, int val2)
 {
+	if (val1==val2)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 	// やること：この処理を数字を比較するように変更します
-	return false;
+	
 }
 
 // val1 から val3 の任意の組み合わせでsumになる組み合わせは何個ありますか？
 int calc_combinations_3(int sum, int val1, int val2, int val3)
 {
-	// やること：val1～val3の和がsumになる数字の組み合わせの数を計算します
-	return 0;
+	int ret = 0;
+	if (sum == val1) ret+=1;
+	if (sum == val2) ret+=1;
+	if (sum == val3) ret += 1;
+	if (sum == val1+val2) ret += 1;
+	if (sum == val1+val3) ret += 1;
+	if (sum == val2+val3) ret += 1;
+	if (sum == val1 + val2 + val3) ret += 1;
+
+
+	return ret;
 }
