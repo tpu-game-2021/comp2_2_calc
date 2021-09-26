3828927fd1255a1f28d1f595ecbfcbda7839324d
zakiAKR <zakican.ak@gmail.com>
2021-09-26 21:20:03 +0900

    テストを通しました。

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -9,12 +9,47 @@
 bool is_same(int val1, int val2)
 {
 	// やること：この処理を数字を比較するように変更します
-	return false;
+	return is_same(val1, val2);
 }
 
 // val1 から val3 の任意の組み合わせでsumになる組み合わせは何個ありますか？
 int calc_combinations_3(int sum, int val1, int val2, int val3)
 {
 	// やること：val1～val3の和がsumになる数字の組み合わせの数を計算します
-	return 0;
+	int combination = 0;
+	int A = val1 + val2;
+	int B = val2 + val3; 
+	int C = val3 + val1;
+	int D = val1 + val2 + val3;
+	
+	if (val1==sum)
+	{
+		combination = combination + 1;
+	}
+	if (val2==sum)
+	{
+		combination = combination + 1;
+	}
+	if (val3==sum)
+	{
+		combination = combination + 1;
+	}
+	if (A==sum)
+	{
+		combination = combination + 1;
+	}
+	if (B==sum)
+	{
+		combination = combination + 1;
+	}
+	if (C==sum)
+	{
+		combination = combination + 1;
+	}
+	if (D==sum)
+	{
+		combination = combination + 1;
+	}
+	
+	return combination;
 }
